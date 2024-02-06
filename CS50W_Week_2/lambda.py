934b0b11cbc5366b89e397f141d8470b10c04015
fnjsk37 <bites-era-00@icloud.com>
2024-02-06 00:19:13 -0600

    Fixed issues with merging

--- CS50W_Week_2/lambda.py
+++ CS50W_Week_2/lambda.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 people = [
     {"name": "Harry", "house": "Gryffindor"},
     {"name": "Cho", "house": "Ravenclaw"},
@@ -12,19 +11,4 @@ def f(person):
 people.sort(key=lambda person: person["house"])
 
 people.sort(key=f)
-=======
-people = [
-    {"name": "Harry", "house": "Gryffindor"},
-    {"name": "Cho", "house": "Ravenclaw"},
-    {"name": "Draco", "house": "Slytherin"}
-]
-
-def f(person):
-    return person["name"]
-
-# Alternatively you can use a one liner with lamda
-people.sort(key=lambda person: person["house"])
-
-people.sort(key=f)
->>>>>>> 311c41f4981f8eae31b7e7dce0bbaa6c0c4caddb
 print(people)
